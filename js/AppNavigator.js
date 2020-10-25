c118a4a487344cb40d37d860badde5fa2d3d8197
vanne15 <vanne@freenet.de>
2020-10-25 10:00:13 +0100

    kürzere schreibweise

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -12,34 +12,38 @@ const Tab = createBottomTabNavigator();
 export default function AppNavigator() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={({ route }) => {
+                    return {
+                        tabBarIcon: ({ focused, size, color }) => {
+                            let icon;
+                            if (route.name === "Home")
+                                icon = focused ? "home" : "home-outline";
+                            if (route.name === "Settings")
+                                icon = focused ? "settings" : "settings-outline";
+                            return (
+                                <Icon.MaterialCommunityIcons
+                                    name={icon}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        }
+                    }
+                }}
+            >
                 <Tab.Screen
                     name="Home"
                     component={HomeScreen}
                     options={{
-                        title: 'Freunde',
-                        tabBarIcon: (object) => {
-                            const { focused, size, color } = object; //Destruierendes Object
-                            const icon = focused ? 'home' : 'home-outline';
-                            return (<Icon.MaterialCommunityIcons
-                                name={icon}
-                                size={size}
-                                color={color}
-                            />); //Für den Fall das man mal mehere Zeilen benötigt (Beispiel weiter unten einzeilig)
-                        }
+                        title: 'Freunde'
                     }}
                 />
                 <Tab.Screen
                     name="Settings"
                     component={SettingsScreen}
                     options={{
-                        title: 'Einstellungen',
-                        tabBarIcon: ({ focused, size, color }) =>
-                            <Icon.MaterialIcons
-                                name="settings"
-                                size={size}
-                                color={color}
-                            />
+                        title: 'Einstellungen'
                     }}
                 />
             </Tab.Navigator>
