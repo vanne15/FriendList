4995810d5f647bf444ab3a60f211ec198aa48304
vanne15 <vanne@freenet.de>
2020-10-25 10:52:17 +0100

    FlatList hinzugefügt

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 export default class Home extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Home / Freunde!</Text>
-                <Button title="gehe zu Alice!" onPress={() =>
-                    this.props.navigation.navigate('FriendScreen', { friend: 'Alice' })} />
-                <Button title="gehe zu Bob!" onPress={() =>
-                    this.props.navigation.navigate('FriendScreen', { friend: 'Bob' })} />
+                <FlatList
+                    data={[
+                        { key: '1', name: 'Alice' },
+                        { key: '2', name: 'Bob' },
+                        { key: '3', name: 'Claudia' },
+                        { key: '4', name: 'Doro' },
+                    ]}
+                    renderItem={({ item }) => (<Button title={`gehe zu ${item.name}`} onPress={() =>
+                        this.props.navigation.navigate('FriendScreen', { friend: item.name })} />)}
+                />
+
             </View>
         );
 
@@ -19,7 +25,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: 30
     },
 });
