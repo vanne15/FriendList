034c78580b9070dc7216490340991bc1c08317c7
vanne15 <vanne@freenet.de>
2020-10-25 11:20:37 +0100

    SectionList hinzugekommen

--- js/screens/SettingsScreen.js
+++ js/screens/SettingsScreen.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 export default class Settings extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Einstellungen!</Text>
+                <SectionList
+                    sections={[
+                        {
+                            title: 'Abschnitt Obst',
+                            data: [
+                                { name: 'Ananas' },
+                                { name: 'Banane' },
+                                { name: 'Clementine' },
+                                { name: 'Dattel' },
+                            ]
+                        },
+                        {
+                            title: 'Abschnitt Länder',
+                            data: [
+                                { name: 'Albanien' },
+                                { name: 'Belgien' },
+                                { name: 'China' },
+                                { name: 'Dänemark' },
+                            ]
+                        }
+                    ]}
+                    renderItem={({ item }) => <Text>{item.name}</Text>}
+                    renderSectionHeader={({ section }) => <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{section.title}</Text>}
+                />
             </View>
         );
 
@@ -17,5 +40,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        paddingTop: 30
     },
 });
